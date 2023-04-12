8f1d080c192e3a0bb060380822468fae467d007e
tienngo <tien0tieng@gmail.com>
2023-04-12 08:54:17 +0700

    Updated .gitignore file

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tnttrips": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "tnttrips904bae71": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "tntTrips220223": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
